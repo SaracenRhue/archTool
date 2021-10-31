9b733f56b734d1c444b2b9a98f4164f072e9fc34
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2021-10-31 23:23:33 +0100

    cleanup

--- archTool.sh
+++ archTool.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# curUsr=$(whoami)
-
 update () {
     yay -Sy 
     yay -Su 
@@ -22,83 +20,22 @@ i_yay () {
 
 clean_gnome () {
     yay -R totem
-}
-
-u_python () {
-    yay -S python3 
-    yay -S python-pip 
-}
-
-i_albert () {
-    yay -S albert-bin
-}
-
-i_brave () {
-    git clone https://aur.archlinux.org/brave-bin.git
-    cd brave-bin
-    makepkg -si
-    sudo rm -fr brave-bin
-}
-i_celluloid () { yay -S celluloid ; }
-i_discord () { yay -S discord ; }
-i_firefox () { yay -S firefox ; }
-i_gnome_boxes () { yay -S gnome-boxes ; }
-i_gparted () { yay -S gparted ; }
-i_grub-customizer () { yay -S grub-customizer ; }
-i_kazam () { yay -S kazam ; }
-i_micro () { 
     yay -R nano
-    yay -S micro
-}
-i_neofetch () {
-    yay -S neofetch 
-    echo "neofetch" >> ~/.bashrc
-    source ~/.bashrc
 }
 
-i_nextcloud () { yay -S nextcloud-client ; }
-i_tor () { yay -S tor-browser ; }
-i_terminator () { yay -S terminator ; }
-i_thunderbird () { yay -S thunderbird ; }
-i_timeshift () { yay -S timeshift ; }
-i_tree () { yay -S tree ; }
-i_celluloid () { yay -S celluloid ; }
-i_vs-code () { yay -S visual-studio-code-bin ; }
 
-
-i_node () {
-    yay -S nodejs 
-    yay -S npm 
-}
-
-cust_grub () {
-    git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
-    cd Top-5-Bootloader-Themes
-    sudo ./install.sh
-    cd
-    sudo rm -fr Top-5-Bootloader-Themes
-}
-
-i_py3 () {
-    yay -S python3 
-    yay -S python-pip 
-    echo "alias python=python3" >> ~/.bashrc
-    source ~/.bashrc
-}
 
 aliases () {
+    echo "neofetch" >> ~/.bashrc
     echo "alias i='yay -S'" >> ~/.bashrc
     echo "alias r='yay -R'" >> ~/.bashrc
     echo "alias s='yay -Ss'" >> ~/.bashrc
-    echo "alias tool='sudo bash debTool.sh'" >> ~/.bashrc
+    echo "alias tool='sudo bash archTool.sh'" >> ~/.bashrc
     echo "done..."
     source ~/.bashrc
 }
 
 theme_tweaks() {
-    yay -S chrome-gnome-shell
-    yay -S curl
-    yay -S wget
     cd Pictures
     wget http://wallpoper.com/images/00/39/10/60/linux-arch_00391060.png
     wget https://wallpapercave.com/wp/NyaITD5.png
@@ -118,10 +55,49 @@ theme_tweaks() {
 
 }
 
+cust_grub () {
+    git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes
+    cd Top-5-Bootloader-Themes
+    sudo ./install.sh
+    cd
+    sudo rm -fr Top-5-Bootloader-Themes
+}
+
 quit () {
     yay -Qdt 
     exit
 }
+
+PKGS = (
+    'python3'
+    'python-pip'
+    'celluloid'
+    'discord'
+    'firefox'
+    'gnome-boxes'
+    'gparted'
+    'grub-customizer'
+    'neofetch'
+    'nodejs'
+    'npm'
+    'kazam'
+    'micro'
+    'visual-studio-code-bin'
+    'thunderbird'
+    'nextcloud-client'
+    'tree'
+    'tor-browser'
+    'chrome-gnome-shell'
+    'curl'
+    'wget'
+)
+i_packages() {
+    for PKG in "${PKGS[@]}"; do
+        yay -S --noconfirm $PKG
+    done
+}
+
+
 
 while :
 do
@@ -139,27 +115,12 @@ EOF
     case "$REPLY" in
         "1") i_yay
              update
+             i_packages
              clean_gnome
-             i_celluloid
-             i_discord
-             i_firefox
-             i_gnome_boxes
-             i_gparted
-             i_grub-customizer
-             i_micro
-             i_neofetch
-             i_nextcloud
-             i_terminator
-             i_tor
-             i_timeshift
-             i_thunderbird
-             i_tree
-             i_vs-code
-             i_node
-             i_py3
              aliases 
+             theme_tweaks
              ;;
-        "2") i_nvidia ;;
+        "2") yay -S --noconfirm nvidia ;;
         "3") cust_grub ;;
         "0") quit && break ;;
      * )  echo "invalid option" ;;
