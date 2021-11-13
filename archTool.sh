22850046c5af41230b2f1634f8e2ade1b312c0ba
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2021-11-13 14:58:01 +0100

    parallel downloading

--- archTool.sh
+++ archTool.sh
@@ -25,36 +25,22 @@ clean_gnome () {
 
 
 
-aliases () {
+bash_aliases () {
+    echo "neofetch" >> ~/.bashrc
+    echo "alias i='yay -S --noconfirm'" >> ~/.bashrc
+    echo "alias r='yay -R --noconfirm'" >> ~/.bashrc
+    echo "alias s='yay -Ss'" >> ~/.bashrc
+    echo "done..."
+    source ~/.bashrc
+}
+
+zsh_aliases () {
     echo "neofetch" >> ~/.zshrc
     echo "alias i='yay -S --noconfirm'" >> ~/.zshrc
     echo "alias r='yay -R --noconfirm'" >> ~/.zshrc
     echo "alias s='yay -Ss'" >> ~/.zshrc
     echo "done..."
     source ~/.zshrc
-}
-
-theme_tweaks() {
-    yay -S --noconfirm gnome-tweaks
-    yay -S --noconfirm chrome-gnome-shell
-    yay -S --noconfirm gnome-shell-extension-dash-to-dock
-    cd Pictures
-    wget http://wallpoper.com/images/00/39/10/60/linux-arch_00391060.png
-    wget https://wallpapercave.com/wp/NyaITD5.png
-    wget https://wallpapercave.com/wp/PKt7K9T.jpg
-    wget https://wallpaperset.com/w/full/2/a/0/418263.jpg
-    cd
-    gsettings set org.gnome.mutter center-new-windows 'true'
-    gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
-    gsettings set org.gnome.desktop.background picture-uri 'file:///home/saracen/Pictures/PKt7K9T.jpg'
-    mv ~/themes/.themes ~/
-    mv ~/themes/.icons ~/
-    gsettings set org.gnome.desktop.interface gtk-theme "Orchris-green-dark"
-    gsettings set org.gnome.desktop.wm.preferences theme "Orchris-green-dark"
-    gsettings set org.gnome.desktop.interface icon-theme "Kora"
-    gsettings set org.gnome.desktop.interface cursor-theme 'macOSBigSur'
-    sudo rm -fr themes
-
 }
 
 cust_grub () {
@@ -75,7 +61,6 @@ r_packages(){
     PKGS=(
     'yelp'
     'python'
-    'firefox-esr'
     'chromium'
     'totem'
     'nano'
@@ -87,66 +72,18 @@ r_packages(){
 
 }
 
-i_packages() {
-    PKGS=(
-    'arc'
-    'albert'
-    'grub'
-    'grub-btrfs'
-    'bash-completion'
-    'python3'
-    'python-pip'
-    'celluloid'
-    'discord'
-    'firefox'
-    'gnome-boxes'
-    'gparted'
-    'htop'
-    'grub-customizer'
-    'neofetch'
-    'nodejs'
-    'npm'
-    'kazam'
-    'micro'
-    'visual-studio-code-bin'
-    'thunderbird'
-    'nextcloud-client'
-    'timeshift'
-    'tree'
-    'tor-browser'
-    'zoom'
-    'github-desktop-bin'
-    'curl'
-    'wget'
-    'zsh'
-    'zsh-autosuggestions'
-    'zsh-syntax-highlighting'
-    )
-
-    for PKG in "${PKGS[@]}"; do
-        yay -S --noconfirm $PKG
-    done
+basic_packages() {
+    yay -R --noconfirm python
+    yay -S --noconfirm arc albert grub grub-btrfs bash-completion python3 python-pip celluloid discord firefox gparted htop grub-customizer neofetch nodejs npm visual-studio-code-bin thunderbird nextcloud-client timeshift tree tor-browser zoom github-desktop-bin curl wget
 }
 
-default_zsh() {
+setup_zsh() {
+    yay -S --noconfirm zsh zsh-autosuggestions zsh-syntax-highlighting
     chsh -s $(which zsh)
 }
 
 i_pip_mods(){
-    PKGS=(
-    'eel'
-    'pyenv'
-    'numpy'
-    'openpyxl'
-    'requests'
-    'selenium'
-    'pyautogui'
-    'beautifulsoup4'
-    )
-
-    for PKG in "${PKGS[@]}"; do
-        pip install $PKG
-    done
+    pip install eel pyenv numpy openpyxl requests selenium pyautogui beautifulsoup4
 }
 
 i_graphicsdriver() {
@@ -169,27 +106,50 @@ do
     cat<<EOF
     Please enter your choice:
 
-     (1) setup
-     (2) theme tweaks
-     (3) inst cust grub
+     (1) basic packages
+     (2) setup zsh
+     (3) zsh aliases
+     (4) bash aliases
+     (9) cust grub
      (0) quit
     ------------------------------
 EOF
     read -n1 -s
     case "$REPLY" in
         "1") i_yay
-             update
+             basic_packages
              i_graphicsdriver
-             i_packages
-             r_packages
-             default_zsh
-             aliases
              i_pip_mods
              ;;
-        "2") theme_tweaks ;;
-        "3") cust_grub ;;
+        "2") setup_zsh ;;
+        "3") zsh_aliases ;;
+        "4") bash_aliases ;;
+        "9") cust_grub ;;
         "0") quit && break ;;
      * )  echo "invalid option" ;;
     esac
     sleep 1
 done
+
+
+
+
+# theme_tweaks() {
+#     yay -S --noconfirm gnome-tweaks chrome-gnome-shell gnome-shell-extension-dash-to-dock
+#     cd Pictures
+#     wget http://wallpoper.com/images/00/39/10/60/linux-arch_00391060.png
+#     wget https://wallpapercave.com/wp/NyaITD5.png
+#     wget https://wallpapercave.com/wp/PKt7K9T.jpg
+#     wget https://wallpaperset.com/w/full/2/a/0/418263.jpg
+#     cd
+#     gsettings set org.gnome.mutter center-new-windows 'true'
+#     gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:'
+#     gsettings set org.gnome.desktop.background picture-uri 'file:///home/saracen/Pictures/PKt7K9T.jpg'
+#     mv ~/themes/.themes ~/
+#     mv ~/themes/.icons ~/
+#     gsettings set org.gnome.desktop.interface gtk-theme "Orchris-green-dark"
+#     gsettings set org.gnome.desktop.wm.preferences theme "Orchris-green-dark"
+#     gsettings set org.gnome.desktop.interface icon-theme "Kora"
+#     gsettings set org.gnome.desktop.interface cursor-theme 'macOSBigSur'
+#     sudo rm -fr themes
+#}
